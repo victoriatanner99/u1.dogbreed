7f02d7d366844c5258ae87cd20b473e61ee0ce41
victoriatanner99 <victoriatanner9915@hotmail.com>
2023-02-20 16:30:27 +0100

    quiz ändringar

--- js/quiz.js
+++ js/quiz.js
@@ -5,8 +5,22 @@ async function get_dog(dog_object_link) {
 
   console.log(correct_dog);
 
-  document.querySelector("#dog_image").style.backgroundImage = `url(${correct_dog.message})`;
+  document.querySelector(
+    ".dog_image"
+  ).style.backgroundImage = `url(${correct_dog.message})`;
 }
 
 get_dog();
+
+
+
+function get_random_dogname () {
+    document.querySelectorAll(".box").forEach(new_name => {
+        let random_number = Math.floor(Math.random() * ALL_BREEDS.length)
+        let dog_name = ALL_BREEDS[random_number];
+        new_name.textContent = dog_name.name;
+    });   
+}
+
+get_random_dogname();
 
